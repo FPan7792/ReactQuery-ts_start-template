0ca9bc3ad1477d4384fb216e39e47b6a116680f3
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2022-11-04 14:48:24 +0100

    setup tools ok ready to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Template REACT_QUERY TS</h1>
-      <p>Welcome</p>
-      <p>React Query ecosystem and devtools already setup</p>
-      <p>Have fun !</p>
-    </div>
+    <>
+      <div className="App">
+        <h1>Template REACT_QUERY TS</h1>
+        <p>Welcome</p>
+        <p>React Query ecosystem and devtools already setup</p>
+        <p>Have fun !</p>
+      </div>
+      <Component />
+    </>
   );
 }
 
 export default App;
+
+const Component = () => {
+  const todoStore = useQuery({ queryKey: ["todos"], queryFn: getTodos });
+  const { data } = todoStore;
+
+  return (
+    <div className="component">
+      <h1>My toDoList</h1>
+
+      {data && data.map((todo: string, index: number) => <p>{todo}</p>)}
+    </div>
+  );
+};
+
+const getTodos = (): string[] => {
+  const todos: string[] = ["Clean the desk", "Get the dog out"];
+  return todos;
+};
